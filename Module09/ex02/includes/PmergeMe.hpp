862d75d3742b1c5aea38424a94b3b09b28835819
misticwave <mgranate_ls@hotmail.com>
2023-09-22 02:01:49 +0100

    Modules Finished :)

--- Module09/ex02/includes/PmergeMe.hpp
+++ Module09/ex02/includes/PmergeMe.hpp
@@ -9,15 +9,18 @@
 # include <cstdlib>
 # include <ctime>
 # include <string>
+# include <chrono>
+# include <iomanip>
 
 class	PmergeMe {
 	private:
+		int				_size;
 		std::list<int>	_myList;
 		std::list<int>	_sortedList;
-		std::deque<int>	_myDeck;
+		std::deque<int>	_secondDeck;
+		std::deque<int>	_firstDeck;
 		std::string		_args;
 		std::list<std::pair<int,int> > _listPair;
-		std::deque<std::pair<int,int> > _dequePair;
 
 	public:
 		PmergeMe( std::string data );
@@ -27,12 +30,23 @@ class	PmergeMe {
 		std::list<int>	getStack( void ) const;
 		std::string		getArgs( void ) const;
 		bool			checkDigit(std::string str);
+		void			insertionList( void );
+		void			insertionDeque( void );
 		void			parseAndAddToLists( void );
 		void			executer( void );
 		void			parseNb( void );
 		void			printLists( void );
-		void			mergeInsert( void );
+		void			mergeInsertList( void );
+		void			mergeInsertDeque( void );
 		void			printPairs( void );
-};		
+		template <typename T>
+		static std::string	printer(T const &c)
+		{
+			std::stringstream ss;
+			for (typename T::const_iterator it = c.begin(); it != c.end(); it++)
+				ss << *it << " ";
+			return ss.str();
+		}
+};
 
 #endif
--- Module09/ex02/srcs/PmergeMe.cpp
+++ Module09/ex02/srcs/PmergeMe.cpp
@@ -2,14 +2,12 @@
 
 PmergeMe::PmergeMe( std::string data ) : _args(data) {
 	std::cout << "\e[0;33mParameter Constructor called of PmergeMe \e[0m" << std::endl;
-	std::cout << "------Original Data-----" << std::endl << data << std::endl;
-	if (!checkDigit(_args))
-	{
+	//std::cout << "------Original Data-----" << std::endl << data << std::endl;
+	if (!checkDigit(_args)) {
 		std::cout << "Error: Wrong format argument" << std::endl;
 		return ;
 	}
 	parseAndAddToLists();
-	printLists();
 	executer();
 }
 
@@ -64,25 +62,56 @@ void	PmergeMe::parseAndAddToLists( void ) {
 		}
         if (std::istringstream(token) >> num) {
             _myList.push_back(num);
-            _myDeck.push_back(num);
+            _firstDeck.push_back(num);
         }
     }
+	_size = _myList.size();
 }
 
 void	PmergeMe::printLists( void ) {
 	std::cout << "---------List-----------" << std::endl;
-	std::list<int>::iterator it = _myList.begin();
-	for ( ; it != _myList.end(); ++it)
+	std::list<int>::iterator it = _sortedList.begin();
+	for ( ; it != _sortedList.end(); ++it)
 		std::cout << *it << " ";
 	std::cout << std::endl;
-	std::cout << "---------Deck-----------" << std::endl;
-	 for (size_t i = 0; i < _myDeck.size(); ++i) {
-        std::cout << _myDeck[i] << " ";
+	std::cout << "---------Deck 1-----------" << std::endl;
+	 for (size_t i = 0; i < _firstDeck.size(); ++i) {
+        std::cout << _firstDeck[i] << " ";
     }
-	std::cout << std::endl;
+	/*std::cout << std::endl;
+	std::cout << "---------Deck 2-----------" << std::endl;
+	 for (size_t i = 0; i < _secondDeck.size(); ++i) {
+        std::cout << _secondDeck[i] << " ";
+    }
+	std::cout << std::endl;*/
 }
 
-void PmergeMe::mergeInsert( void )
+void PmergeMe::insertionList()
+{
+    for (std::list<std::pair<int, int>>::iterator it = _listPair.begin(); it != _listPair.end(); ++it)
+    {
+        if (_sortedList.empty()) {
+            _sortedList.push_back(it->first);
+            _sortedList.push_back(it->second);
+        }
+        else {
+            std::list<int>::iterator it2 = _sortedList.begin();
+            while (it2 != _sortedList.end() && *it2 < it->first) {
+                ++it2;
+            }
+            _sortedList.insert(it2, it->first);
+
+            it2 = _sortedList.begin();
+            while (it2 != _sortedList.end() && *it2 < it->second) {
+                ++it2;
+            }
+            _sortedList.insert(it2, it->second);
+        }
+    }
+    //printLists();
+}
+
+void PmergeMe::mergeInsertList( void )
 {
     for (std::list<int>::iterator it = _myList.begin(); it != _myList.end(); ) {
         if (_myList.size() == 1) {
@@ -101,11 +130,92 @@ void PmergeMe::mergeInsert( void )
 		else
 			_sortedList.push_back(currentElement);
     }
-	printPairs();
+	insertionList();
 }
+
+void PmergeMe::insertionDeque( void )
+{
+	if (_secondDeck.size() % 2 != 0) {
+		_firstDeck.push_front(_secondDeck.back());
+		_secondDeck.pop_back();
+	}
+	else {
+		_firstDeck.push_back(_secondDeck.back());
+		_firstDeck.push_front(_secondDeck.front());
+		_secondDeck.pop_front();
+		_secondDeck.pop_back();
+	}
+	while (_secondDeck.size() > 0) {
+		std::deque<int>::iterator it = _firstDeck.begin();
+		while (it != _firstDeck.end() && *it < _secondDeck.front()) {
+			++it;
+		}
+		_firstDeck.emplace(it, _secondDeck.front());
+		//_firstDeck.insert(it2, _secondDeck.front());
+		_secondDeck.pop_front();
+
+		while (it != _firstDeck.end() && *it < _secondDeck.back()) {
+			++it;
+		}
+		_firstDeck.emplace(it, _secondDeck.back());
+		//_firstDeck.insert(it, _secondDeck.back());
+		_secondDeck.pop_back();
+	}
+}
+
+/*
+myDeque.push_back(4);
+myDeque.push_front(0);
+myDeque.pop_back();
+myDeque.pop_front();
+*/
+
+void PmergeMe::mergeInsertDeque( void )
+{
+	std::deque<int>::iterator it = _firstDeck.begin();
+	if (_firstDeck.size() == 1) {
+		_secondDeck.push_front(*it);
+		return;
+	}
+ 	for (; it != _firstDeck.end(); ) {
+		int currentElement = *it;
+		++it;
+		if (it != _firstDeck.end()) {
+			if (*it > currentElement) {
+				_secondDeck.push_back(*it);
+				_secondDeck.push_front(currentElement);
+			}
+			else {
+				_secondDeck.push_back(currentElement);
+				_secondDeck.push_front(*it);
+			}
+			it++;
+		}
+		else
+			_secondDeck.push_back(currentElement);
+	}
+	_firstDeck.clear();
+	insertionDeque();
+}
+
 void	PmergeMe::executer( void )  {
-	mergeInsert();
+	long double deckTime;
+	long double listTime;
+
+	std::cout << "Before: " << printer(_myList) << std::endl;
+	std::cout << "After: " << printer(_firstDeck) << std::endl;
+	std::clock_t start = std::clock();
+	mergeInsertList();
+	listTime = (double)((std::clock() - start) / (double)(CLOCKS_PER_SEC / 1000));
+	start = std::clock();
+	mergeInsertDeque();
+	deckTime = (double)((std::clock() - start) / (double)(CLOCKS_PER_SEC / 1000));
+	//printLists();
+	std::cout << "Time to process a range of " << _size << " elements with std::deck: " << deckTime << " ms" << std::endl;
+	std::cout << "Time to process a range of " << _size << " elements with std::list: " << listTime << " ms" << std::endl;
 }
+
+// Function that returns the timestamp()
 
 void	PmergeMe::printPairs()
 {
